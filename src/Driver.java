4557c72a2640beee120c8485763c0d45bc119f0d
Chenry <henrychen0214@gmail.com>
2025-02-12 02:01:05 -0500

    pe4 done

---
+++ src/Driver.java
@@ -0,0 +1,17 @@
+
+public class Driver {
+    public static void main(String[] args) {
+        Food[] meal1 = TechDining.createMeal(5);
+        Food[] meal2 = TechDining.createMeal(2);
+        int cost1 = TechDining.mealCost(meal1);
+        int cost2 = TechDining.mealCost(meal2);
+        System.out.println("Cost of meal 1: " + cost1);
+        System.out.println("Cost of meal 2: " + cost2);
+        Food[][] order1 = TechDining.createOrder(3);
+        Food[][] order2 = TechDining.createOrder(4);
+        int orderCost1 = TechDining.orderCost(order1);
+        int orderCost2 = TechDining.orderCost(order2);
+        System.out.println("Cost of order 1: " + orderCost1);
+        System.out.println("Cost of order 2: " + orderCost2);
+    }
+}
--- src/TechDining.java
+++ src/TechDining.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class TechDining {
-    static Food[] createMeal(int lengthMeal) {
+    public static Food[] createMeal(int lengthMeal) {
         Food[] meal = new Food[lengthMeal];
         Random rand = new Random();
         for (int i = 0; i < lengthMeal; i++) {
@@ -11,7 +11,7 @@ public class TechDining {
         return meal;
     }
 
-    static Food[][] createOrder(int lengthOrder) {
+    public static Food[][] createOrder(int lengthOrder) {
         Food[][] output = new Food[lengthOrder][lengthOrder];
         int counter = 0;
         for (int i = lengthOrder; i > 0; i--) {
@@ -25,7 +25,7 @@ public class TechDining {
         }
         return output;
     }
-    static int mealCost(Food[] meal) {
+    public static int mealCost(Food[] meal) {
         int cost = 0;
         // System.out.println(" meal ---------------------");
         for (int i = 0; i < meal.length; i++) {
@@ -38,26 +38,12 @@ public class TechDining {
         return cost;
     }
 
-    static int orderCost(Food[][] order) {
+    public static int orderCost(Food[][] order) {
         int cost = 0;
         for (int i = 0; i < order.length; i++) {
             cost += mealCost(order[i]);
         }
         return cost;
     }
-    public static void main(String[] args) {
-        Food[] meal1 = createMeal(5);
-        Food[] meal2 = createMeal(2);
-        int cost1 = mealCost(meal1);
-        int cost2 = mealCost(meal2);
-        System.out.println("Cost of meal 1: " + cost1);
-        System.out.println("Cost of meal 2: " + cost2);
-        Food[][] order1 = createOrder(3);
-        Food[][] order2 = createOrder(4);
-        int orderCost1 = orderCost(order1);
-        int orderCost2 = orderCost(order2);
-        System.out.println("Cost of order 1: " + orderCost1);
-        System.out.println("Cost of order 2: " + orderCost2);
 
-    }
 }
